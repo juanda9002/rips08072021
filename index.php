039ef9d8febaad765787c19ff52f53ca72edade2
juanda9002 <perdomo9002@gmail.com>
2021-04-29 14:27:20 -0500

    prueba modificado

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
  * APPLICATION ENVIRONMENT
  *---------------------------------------------------------------
  *
- * You can load different configurations depending on your
+ * You can load different configurations depending on your ----
  * current environment. Setting the environment also influences
  * things like logging and error reporting.
  *
